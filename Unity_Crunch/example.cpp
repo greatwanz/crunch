4c0282f4e60747025e6ae95a14fca2c5f4535685
Tyng-Yih Lin <tyngyih0@gmail.com>
2018-12-07 00:05:50 +0800

    Fix typo in example

--- Unity_Crunch/example.cpp
+++ Unity_Crunch/example.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char* argv[]) {
 	CrunchObject* crunchObj = new CrunchObject();
 	//crunchObj->compress("C:/Users/greatwanz/Desktop/Newfolder/001.jpg", 500, 730);
-	//crunchObj->decompress_write("C:/greatwanz/TY/Desktop/Newfolder/001.crn");
+	//crunchObj->decompress_write("C:/Users/greatwanz/Desktop/Newfolder/001.crn");
 	delete crunchObj;
 	return EXIT_SUCCESS;
 }
