14db8ffea00efd72e30b8980b5284123ddf81a8f
Tyng-Yih Lin <tyngyih0@gmail.com>
2018-12-07 00:21:22 +0800

    Added C# wrappers

---
+++ Unity_Crunch/Managed/ManagedCrunchObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WS
+{
+    public class ManagedCrunchObject
+    {
+        private IntPtr native_instance_;
+
+        public ManagedCrunchObject()
+        {
+            native_instance_ = ManagedCrunchWrapper.new_CrunchObject();
+        }
+
+        public void delete()
+        {
+            ManagedCrunchWrapper.delete_CrunchObject(native_instance_);
+        }
+
+        public void compress(string filepath, int width = 0, int height = 0)
+        {
+            ManagedCrunchWrapper.compress(native_instance_, filepath, width, height);
+        }
+
+        public IntPtr decompress(string filepath)
+        {
+            return ManagedCrunchWrapper.decompress(native_instance_, filepath);
+        }
+
+        public int get_dds_filesize()
+        {
+            return ManagedCrunchWrapper.get_dds_filesize(native_instance_);
+        }
+    }
+}
---
+++ Unity_Crunch/Managed/ManagedCrunchWrapper.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+namespace WS
+{
+    public static class ManagedCrunchWrapper
+    {
+        [DllImport("Unity_Crunch")]
+        public static extern IntPtr new_CrunchObject();
+
+        [DllImport("Unity_Crunch")]
+        public static extern void delete_CrunchObject(IntPtr instance);
+
+        [DllImport("Unity_Crunch")]
+        public static extern void compress(IntPtr instance, string filepath, int width, int height);
+
+        [DllImport("Unity_Crunch")]
+        public static extern IntPtr decompress(IntPtr instance, string filepath);
+
+        [DllImport("Unity_Crunch")]
+        public static extern int get_dds_filesize(IntPtr instance);
+    }
+}
